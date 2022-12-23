6ab7715c6e2075fbb51b3d4fb82909d3656b6e79
EVDOC88 <vovan8888@mail.ru>
2022-12-23 20:00:44 +0300

    Програмка

--- mod3/Program.cs
+++ mod3/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace mod3
 {
+    using System;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            {
+                string MyName = "Jane";
+                byte MyAge = 27;
+                bool HaveIApet = true;
+                double MyShoeSize = 37.5;
+
+                Console.WriteLine("My name is " + MyName);
+                Console.WriteLine("MyAge " + MyAge);
+                Console.WriteLine("Do I have a pet? " + HaveIApet);
+                Console.WriteLine("My shoe size is " + MyShoeSize);
+            }
+
         }
     }
 }
